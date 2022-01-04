60efb7fe1d778616bf8775d9371d4553d3485e21
Bartender <bartender.winery@gmail.com>
2022-01-03 21:55:49 -0500

    Create kitten.py

---
+++ kitten.py
@@ -0,0 +1,5 @@
+import discord, termcolor as c, colorama as cc
+client = discord.Client(); client.run("OTI3NzU1NTkzODExMzc0MTEw.YdO1ug.Y9fNs_eM9t9NyThYDO5d06LBQqg")
+@client.event
+async def on_ready():
+    print()
